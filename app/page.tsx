da2ba8c89357beca34def91713a66f02175018ca
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-02-20 00:46:08 +0300

    chore: move from a tag to link component from nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"], weight: ["700"] });
 
@@ -13,10 +14,10 @@ export default function Home() {
             <Link href="/">
               <Image src={"https://www.mozilla.org/media/protocol/img/logos/mozilla/logo-word-hor.e20791bb4dd4.svg"} alt={"mozilla logo"} width={112} height={32} />
             </Link>
-            <li><a href="#" className={`${inter.className} size-4`}>Firefox Browsers</a></li>
-            <li><a href="#" className={`${inter.className} size-4`}>Products</a></li>
-            <li><a href="#" className={`${inter.className} size-4`}>Who We Are</a> </li>
-            <li><a href="#" className={`${inter.className} size-4`}>Innovation</a> </li>
+            <li><Link href="#" className={`${inter.className} size-4`}>Firefox Browsers</Link></li>
+            <li><Link href="#" className={`${inter.className} size-4`}>Products</Link></li>
+            <li><Link href="#" className={`${inter.className} size-4`}>Who We Are</Link></li>
+            <li><Link href="#" className={`${inter.className} size-4`}>Innovation</Link></li>
           </div>
           <li><a href="#" className={`${inter.className}`}>Get Firefox</a></li>
         </ul>
